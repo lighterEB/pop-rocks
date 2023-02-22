fb19e5baaaa2c176514e381525dfdcab37a2fd97
lighterEB <hellohus726@126.com>
2023-02-22 17:32:19 +0800

    构建项目

--- src/main/java/com/tosix7/poprocks/mapper/SysUserMapper.java
+++ src/main/java/com/tosix7/poprocks/mapper/SysUserMapper.java
@@ -2,6 +2,8 @@ package com.tosix7.poprocks.mapper;
 
 import com.tosix7.poprocks.model.SysUser;
 
+import java.util.List;
+
 public interface SysUserMapper {
     int deleteByPrimaryKey(Long id);
 
@@ -14,4 +16,10 @@ public interface SysUserMapper {
     int updateByPrimaryKeySelective(SysUser record);
 
     int updateByPrimaryKey(SysUser record);
+
+    /**
+     * 查询所有
+     * @return
+     */
+    List<SysUser> findAll();
 }
