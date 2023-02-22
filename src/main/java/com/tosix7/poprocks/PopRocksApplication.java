4c06e8475091fe257d56a808f3e870c96c64d444
lighterEB <hellohus726@126.com>
2023-02-22 16:39:19 +0800

    构建项目

---
+++ src/main/java/com/tosix7/poprocks/PopRocksApplication.java
@@ -0,0 +1,13 @@
+package com.tosix7.poprocks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PopRocksApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PopRocksApplication.class, args);
+    }
+
+}
