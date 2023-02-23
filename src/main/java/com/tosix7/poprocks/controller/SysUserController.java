20668c1e58bbfe2cba6ff064456670c0bb6a6427
lighterEB <hellohus726@126.com>
2023-02-23 17:50:03 +0800

    构建项目

--- src/main/java/com/tosix7/poprocks/controller/SysUserController.java
+++ src/main/java/com/tosix7/poprocks/controller/SysUserController.java
@@ -1,8 +1,20 @@
 package com.tosix7.poprocks.controller;
 
+import com.tosix7.poprocks.service.SysUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("user")
 public class SysUserController {
 
+    @Autowired
+    private SysUserService sysUserService;
+
+    @GetMapping(value = "/findAll")
+    public Object findAll(){
+        return sysUserService.findAll();
+    }
 }
